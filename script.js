f76148f49c877053bc1682c013646b92e7688642
Majix <184645908+majix5612@users.noreply.github.com>
2025-02-25 15:01:16 +0000

    script.js remodel

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const blocklist = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','
 const allowedlist = ['1','2','3','4','5','6','7','8','9','0'];
 
 function startcode() {
-    console.log("Welcome to the Password Generator");
-    console.log("Your password will be temporary saved to the script before being deleted");
-    console.log("© Caiden Pinter 2025");
-    console.log("Current Version is: ", versioncodename);
-    console.log("Please answer the questions below");
+    alert("Welcome to the Password Generator");
+    alert("Your password will be temporary saved to the script before being deleted");
+    alert("© Caiden Pinter 2025");
+    alert("Current Version is: ", versioncodename);
+    alert("Please answer the questions below");
 
     const readline = require('readline').createInterface({
         input: process.stdin,
@@ -20,18 +20,18 @@ function startcode() {
 
     readline.question("Password Length: ", O => {
         if (O === "") {
-            console.log("Please enter a number do not leave blank");
-            console.log("Please try again by rerunning the script");
+            alert("Please enter a number do not leave blank");
+            alert("Please try again by rerunning the script");
             readline.close();
             startcode();
         } else if (Array.from(O).some(char => blocklist.includes(char))) {
-            console.log("You typed an invalid character please try again");
-            console.log("Please try again");
+            alert("You typed an invalid character please try again");
+            alert("Please try again");
             readline.close();
             startcode();
         } else if (Array.from(O).some(char => allowedlist.includes(char))) {
             const q = parseInt(O, 10);
-            console.log("Your password will contain numbers letters");
+            alert("Your password will contain numbers letters");
             let l = Array.from({ length: q }, () => String.fromCharCode(65 + Math.floor(Math.random() * 26)));
             let y = Array.from({ length: q }, () => Math.floor(Math.random() * 10).toString());
             let b = Array.from({ length: q }, () => String.fromCharCode(97 + Math.floor(Math.random() * 26)));
@@ -43,24 +43,24 @@ function startcode() {
             let p2 = y;
             let p3 = b;
             passwordlist = Array.from(p1 + p2 + p3 + p4);
-            console.log("Current Vars (Unshuffled Variables available for password generation | Unformatted text): ");
-            console.log(passwordlist);
+            alert("Current Vars (Unshuffled Variables available for password generation | Unformatted text): ");
+            alert(passwordlist);
             passwordlist = passwordlist.sort(() => Math.random() - 0.5);
-            console.log("Final Password: ");
+            alert("Final Password: ");
             let final_password = Array.from({ length: q }, () => passwordlist[Math.floor(Math.random() * passwordlist.length)]).join('');
-            console.log(final_password.slice(0, q));
-            console.log("Thank you for using this tool!");
-            console.log("Your password has been cleared");
-            console.log("A problem has occurred please try again");
+            alert(final_password.slice(0, q));
+            alert("Thank you for using this tool!");
+            alert("Your password has been cleared");
+            alert("A problem has occurred please try again");
             readline.close();
             startcode();
         } else if (O === "0") {
-            console.log("Please use a higher number");
+            alert("Please use a higher number");
             readline.close();
             startcode();
         } else {
-            console.log("E10: The input given is invalid use numbers only");
-            console.log("Either there was a code fault or was a user error.\nA critical error occurred");
+            alert("E10: The input given is invalid use numbers only");
+            alert("Either there was a code fault or was a user error.\nA critical error occurred");
             readline.close();
             process.exit(1);
         }
