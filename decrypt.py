1f69520a29083a4a769425a423edeb288d81e7b8
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-03 17:54:31 -0500

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -7,7 +7,7 @@ print("4/5 | Decrypting files")
 def decrypt2():
     i = "y"
     if i == "y":
-        print("\nDecrypting 1/2")
+        print("\nDecrypting 2/2")
         with open('password3key.key', 'rb') as filekey:
             key = filekey.read()
             fernet = Fernet(key)
@@ -16,7 +16,7 @@ def decrypt2():
                 decrypted = fernet.decrypt(encrypted)
                 with open('passwordgen3.py', 'wb') as dec_file:
                     dec_file.write(decrypted)
-                    print("\nMaking backup of old key")
+                    #print("\nMaking backup of old key")
                     f = open('password3key.key', 'r')
                     open('encbackup.key', 'x')
                     v = open('encbackup.key', 'w')
@@ -31,7 +31,7 @@ def decrypt2():
                     elif i == "n":
                         os.remove('password3key.key')
                         os.remove('encbackup.key')
-                        print("Thank you for using eCrypt")
+                        #print("Thank you for using eCrypt")
                         os.system('python3 cleanup.py')
                         exit()
                         
