622de550ced6df4893f92b3efec650d825b620ac
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-03 18:31:20 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo Majix Installer for eCrypt
-echo Please enter password below to install curl
-sudo apt-get install curl
-curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/install.py -o install.py
-python3 install.py
