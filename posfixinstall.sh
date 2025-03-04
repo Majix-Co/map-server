03ad1977610906c6d311b955b83c666badac0fdb
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-04 11:59:12 -0500

    Update posfixinstall.sh

--- posfixinstall.sh
+++ posfixinstall.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 echo eCrypt Self Extracting Script
 echo 1/5 | Downloading needed files
-echo \nPreparing for deployment\n
-echo \n//////////////////////Preparing Self Extracting Script////////////////////////////////\n
-sleep 5
+echo Preparing for deployment
+echo //////////////////////Preparing Self Extracting Script////////////////////////////////
+sleep 3
 echo Getting needed files
+echo Please enter password to install Curl
 sudo apt-get install curl
 cd eCrypt-Installer
 echo Getting files
 curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/eCrypt4.1.py -o eCrypt4.1.py
-sleep 5
-echo \nDetected as encrypted\n
+sleep 3
+echo Detected as encrypted
 curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/passwordgen3.py -o passwordgen3.py
-echo \nSwitching back to python\n
-sleep 5
+echo Switching back to python
+sleep 3
 curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/eCrypt4.1.py -o eCryptcm.py
 curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/passwordgen3.py -o passwordgencm.py
 curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/posfixinstall2.py -o posfixinstall2.py
