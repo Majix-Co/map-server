399726eeb68b23a39a80f1cc6f23d389134ee235
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-05 22:58:43 +0000

    d

--- ntscript/cleanup.bat
+++ ntscript/cleanup.bat
@@ -3,6 +3,4 @@ set olddirectory=%CD%
 cd ..
 set currentworking=%CD%
 del /f posfixinstall.bat
-curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/ntscript/cleanup2.bat -o cleanup2.bat
-call cleanup2.bat
 exit
