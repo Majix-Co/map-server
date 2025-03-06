39c78a93040066dd371ed6d3762bc806b6775cca
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-06 22:33:25 +0000

    Postfix NT Patch

--- ntscript/postfixinstall3.py
+++ ntscript/postfixinstall3.py
@@ -3,7 +3,7 @@ import shutil
 mainvar = open('tempfile.txt', 'r')
 tempvar = mainvar.read().strip()
 mainvar.close()
-dir = os.getcwd() + "\\"
+dir = os.getcwd() + "\\" + "eCrypt-Installer"
 print("////////////////////////////////////////////[Majix Installer]//////////////////////////////////////////")
 print("3/5 | Preparing for Decryption")
 os.system('curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/ecryptinkey.key -o ecryptinkey.key')
