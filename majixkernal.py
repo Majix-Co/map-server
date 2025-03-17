d784ccc794f6557baf2f38c5163704b7e9e59e87
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-17 14:24:57 +0000

    Cleared dierctory for majix OnBase installer tester

---
+++ majixkernal.py
@@ -0,0 +1,21 @@
+print("Majix Kernal 4.4")
+print("Server side only")
+print("BETA SOFTWARE")
+print("Jix Kernal 4.4")
+print("Getting required kernal files please wait")
+import os
+import time
+import dateutil
+import shutil
+print("Getting current working directory")
+workingdirectory = os.getcwd()
+print("Getting Current local times")
+print("Jix Boot Manager")
+i = input('mb>')
+if i == "boot kernal":
+    print("Okay booting kernal")
+    
+
+
+elif i == "muf check":
+    print("Getting files")
