9b3ce99459b4d5c4a52db7482a0355a50895e3ac
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-19 09:44:13 -0400

    Update postfixinstall3.py

--- ntscript/postfixinstall3.py
+++ ntscript/postfixinstall3.py
@@ -6,9 +6,9 @@ mainvar.close()
 dir = os.getcwd() + "\\" + "eCrypt-Installer" + "\\"
 print("////////////////////////////////////////////[Majix Installer]//////////////////////////////////////////")
 print("3/5 | Preparing for Decryption")
-os.system('curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/ecryptinkey.key -o ecryptinkey.key')
+os.system('curl https://raw.githubusercontent.com/Majix-Co/map-server/refs/heads/Installmain/ecryptinkey.key -o ecryptinkey.key')
 shutil.move(tempvar + "\\" + "ecryptinkey.key", dir)
-os.system('curl https://raw.githubusercontent.com/Majix-Co/eCrypt-Services/refs/heads/Installmain/password3key.key -o password3key.key')
+os.system('curl https://raw.githubusercontent.com/Majix-Co/map-server/refs/heads/Installmain/password3key.key -o password3key.key')
 shutil.move(tempvar + "\\" + "password3key.key", dir)
 print("\nAlright!, Files were downloaded sucsefully running python code")
 run = "python3" + " " + dir + "decrypt.py"
