7a22aa28128cf79aa108e3a5a05b2dc887b99e26
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-21 13:03:17 +0000

    License update

--- ntscript/posfixinstall2nt.py
+++ ntscript/posfixinstall2nt.py
@@ -9,33 +9,37 @@ source3 = dir.strip() + "\\" + "license.txt"
 source = dir + "\\" + "eCryptcm.py"
 scheck1 = dir + "\\" + "eCrypt4.4.py"
 license = open(source3, 'r')
-print("Please read the license")
-print(license.read())
-license.close()
-i = input("Do you agree to the license agreement (yes/no[default])")
-if i == "yes":
-    print("Checking values")
-    filecheck = filecmp.cmp(source,scheck1)
-    if filecheck == True:
-        print("\nOkay File looks okay")
-        source = dir + "/" + "passwordgencm.py"
-        scheck1 = dir + "/" + "passwordgen3.py"
-        filecheck2 = filecmp.cmp(source,scheck1)
-        if filecheck2 == True:
-            print("All checks Passed!")
-            print("Preparing next check")
-            run = "python3" + " " + dir + "\\" + "postfixinstall3.py"
-            os.system(run)
+print("Will attempt to print license if fails will go back to python")
+notepad = "notepad.exe " + source3
+try:
+    os.system(notepad)
+except:
+    print(license.read())
+    license.close()
+    i = input("Do you agree to the license agreement (yes/no[default])")
+    if i == "yes":
+        print("Checking values")
+        filecheck = filecmp.cmp(source,scheck1)
+        if filecheck == True:
+            print("\nOkay File looks okay")
+            source = dir + "/" + "passwordgencm.py"
+            scheck1 = dir + "/" + "passwordgen3.py"
+            filecheck2 = filecmp.cmp(source,scheck1)
+            if filecheck2 == True:
+                print("All checks Passed!")
+                print("Preparing next check")
+                run = "python3" + " " + dir + "\\" + "postfixinstall3.py"
+                os.system(run)
+                exit()
+            elif filecheck2 == False:
+                print("Looks like the file is not right")
+                exit()
+        elif filecheck == False:
+            print("File may have been tampered with they do not match killing script")
+            print("This will leave a partial install please clear this directory and rerun the script OR rerun the install.py script in the same diredctory it was originaly in to clear this directory")
             exit()
-        elif filecheck2 == False:
-            print("Looks like the file is not right")
-            exit()
-    elif filecheck == False:
-        print("File may have been tampered with they do not match killing script")
-        print("This will leave a partial install please clear this directory and rerun the script OR rerun the install.py script in the same diredctory it was originaly in to clear this directory")
+    else:
+        print("Sorry you need to agree to the license to install this program")
+        deldir = "rmdir /s /q " + dir
+        os.remove(deldir)
         exit()
-else:
-    print("Sorry you need to agree to the license to install this program")
-    deldir = "rmdir /s /q " + dir
-    os.remove(deldir)
-    exit()
