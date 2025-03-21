83aed488596ca96972311cd67290673ee79a748d
Majix <184645908+majix5612@users.noreply.github.com>
2025-03-21 16:44:25 +0000

    Add GUI license agreement handling and refactor license check logic

--- posfixinstall2.py
+++ posfixinstall2.py
@@ -8,6 +8,76 @@ time.sleep(2)
 dir = os.getcwd()
 dirtest = "eCrypt-Installer"
 source3 = dir + "/" + "license.txt"
+def startgui():
+    #print("Please ready the license below\n")
+    #print("\n")
+    #print(license.read())
+    i = input("\nDo you agree to the license? (yes/no[default])")
+    license.close()
+    os.remove(source3)
+    if i == "yes":
+        source = dir + "/" + "eCryptcm.py"
+        scheck1 = dir + "/" + "eCrypt4.4.py"
+        print("Checking values")
+        filecheck = filecmp.cmp(source,scheck1)
+        if filecheck == True:
+            print("\nOkay File looks okay")
+            source = dir + "/" + "passwordgencm.py"
+            scheck1 = dir + "/" + "passwordgen3.py"
+            filecheck2 = filecmp.cmp(source,scheck1)
+            if filecheck2 == True:
+                print("All checks Passed!")
+                print("Preparing next check")
+                os.system('chmod +x postfixinstall3.sh')
+                os.system('./postfixinstall3.sh')
+                exit()
+            elif filecheck2 == False:
+                print("Looks like the file is not right")
+                exit()
+        elif filecheck == False:
+            print("File may have been tampered with they do not match killing script")
+            exit()
+    else:
+        print("Whoops!, This program can not install if the license was not agreeded to")
+        print("Please rerun the install script")
+        deldir = "rm -rf " + dir
+        os.system(deldir)
+        exit()
+def start():
+    print("Please ready the license below\n")
+    print("\n")
+    print(license.read())
+    i = input("\nDo you agree to the license? (yes/no[default])")
+    license.close()
+    os.remove(source3)
+    if i == "yes":
+        source = dir + "/" + "eCryptcm.py"
+        scheck1 = dir + "/" + "eCrypt4.4.py"
+        print("Checking values")
+        filecheck = filecmp.cmp(source,scheck1)
+        if filecheck == True:
+            print("\nOkay File looks okay")
+            source = dir + "/" + "passwordgencm.py"
+            scheck1 = dir + "/" + "passwordgen3.py"
+            filecheck2 = filecmp.cmp(source,scheck1)
+            if filecheck2 == True:
+                print("All checks Passed!")
+                print("Preparing next check")
+                os.system('chmod +x postfixinstall3.sh')
+                os.system('./postfixinstall3.sh')
+                exit()
+            elif filecheck2 == False:
+                print("Looks like the file is not right")
+                exit()
+        elif filecheck == False:
+            print("File may have been tampered with they do not match killing script")
+            exit()
+    else:
+        print("Whoops!, This program can not install if the license was not agreeded to")
+        print("Please rerun the install script")
+        deldir = "rm -rf " + dir
+        os.system(deldir)
+        exit()
 if dirtest in dir:
     license = open(source3, 'r')
     x = input("Would you like to use a GUI license viewer? (y/n): ")
@@ -18,6 +88,7 @@ if dirtest in dir:
                 print("Okay launching kate")
                 kate = ["kate", source3]
                 subprocess.run(kate, check = True)
+                startgui()
             except:
                 print("Your system could not launch kate will rerun script try using text instead")
                 rerun = "python3 " + __file__
@@ -27,6 +98,7 @@ if dirtest in dir:
                 print("Okay launching gedit")
                 gedit = ["gedit", source3]
                 subprocess.run(gedit, check = True)
+                startgui()
             except:
                 print("Your system could not launch gedit will rerun script try using text instead")
                 rerun = "python3 " + __file__
@@ -36,6 +108,7 @@ if dirtest in dir:
                 print("Okay launching XED")
                 xed = ["xed", source3]
                 subprocess.run(xed, check = True)
+                startgui()
             except:
                 print("Your system could not launch xde will rerun script try using text instead")
                 rerun = "python3 " + __file__
@@ -47,45 +120,13 @@ if dirtest in dir:
                 nano = ["nano", source3]
                 print(nano)
                 subprocess.run(nano, check = True)
+                startgui()
             except:
                 print("Your system could not launch nano will rerun script try using text instead")
                 rerun = "python3 " + __file__
                 os.system(rerun)
     else:
-        print("Please ready the license below\n")
-        print("\n")
-        print(license.read())
-        i = input("\nDo you agree to the license? (yes/no[default])")
-        license.close()
-        os.remove(source3)
-        if i == "yes":
-            source = dir + "/" + "eCryptcm.py"
-            scheck1 = dir + "/" + "eCrypt4.4.py"
-            print("Checking values")
-            filecheck = filecmp.cmp(source,scheck1)
-            if filecheck == True:
-                print("\nOkay File looks okay")
-                source = dir + "/" + "passwordgencm.py"
-                scheck1 = dir + "/" + "passwordgen3.py"
-                filecheck2 = filecmp.cmp(source,scheck1)
-                if filecheck2 == True:
-                    print("All checks Passed!")
-                    print("Preparing next check")
-                    os.system('chmod +x postfixinstall3.sh')
-                    os.system('./postfixinstall3.sh')
-                    exit()
-                elif filecheck2 == False:
-                    print("Looks like the file is not right")
-                    exit()
-            elif filecheck == False:
-                print("File may have been tampered with they do not match killing script")
-                exit()
-        else:
-            print("Whoops!, This program can not install if the license was not agreeded to")
-            print("Please rerun the install script")
-            deldir = "rm -rf " + dir
-            os.system(deldir)
-            exit()
+        start()
 else:
     print('Whoops!, A critical error was detected kill script to save system')
     print('What happened? You are NOT running this in the install directory')
